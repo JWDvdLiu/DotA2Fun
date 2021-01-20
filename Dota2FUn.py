4532e25d5f0c349ae4691df488e04566ca1f3320
DavidLiu1 <42097926+DavidLiu1@users.noreply.github.com>
2021-01-19 23:07:26 -0500

    2

--- Dota2FUn.py
+++ Dota2FUn.py
@@ -1,6 +1,15 @@
-import requests
 import json
+import time
+import requests
 
-r = requests.get("https://api.opendota.com/api/players/125581247/matches")
 
-print(r.text)
+# hero_name_file = open('G:\DAVID\DotA2Fun\heroes.json')
+# heroNames = json.load(hero_name_file)
+# hero_name_file.close
+
+# for hero in heroNames:
+#    print(hero['localized_name'])
+
+r = requests.get("https://api.opendota.com/api/matches/5314309908")
+
+
